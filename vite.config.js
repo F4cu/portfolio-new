13f1351f25b00712ee6606205571e91057ebbee8
F4cu <facundo.rosales@gmail.com>
2026-02-04 12:19:11 +0100

    Adding Upskill component library preview

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         awinProject: './projects/awin-design-system.html',
         datavizProject: './projects/data-visualization-guide.html',
         upskillProject: './projects/upskill-design-system.html',
-        project4: './projects/project-4.html',
+        upskillPreview: './projects/upskill-preview.html',
       }
     }
   }
